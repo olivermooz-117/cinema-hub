b38b10a9db640716c6244e25fb67d1895da6148f
HexCipher224 <mofiamadiback@gmail.com>
2026-05-13 14:53:32 +0300

    update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx - SIMPLE TEST VERSION
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ color: 'white', fontSize: '40px', padding: '20px' }}>
+        Cinema Hub - Testing
+      </h1>
+      <p style={{ color: 'white', padding: '20px' }}>
+        If you can see this text, React is working!
+      </p>
+    </div>
+  )
+}
+
+export default App
