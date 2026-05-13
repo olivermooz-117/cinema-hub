765a4798904284bf8a449d52bebf7404fe2d0ea1
HexCipher224 <mofiamadiback@gmail.com>
2026-05-13 15:38:38 +0300

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container,} from 'react-bootstrap';
 
 const NavigationBar = () => {
   return (
