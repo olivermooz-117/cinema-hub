8e367d58da0c73f6e0a1e1d5498ac1d7c146ee41
Sascha Mckayla <mckaylasascha@gmail.com>
2026-05-14 13:44:33 +0300

    feat: add state for name, email, and tickets

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
 function BookingForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [tickets, setTickets] = useState(1);
   return (
     <div>
       <h1>Book Movie Tickets</h1>
